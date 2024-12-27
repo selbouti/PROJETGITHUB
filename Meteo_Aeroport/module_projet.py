d69a97488c7e430cd227a9240cef060d7b35ad53
etudiant <etudiant@debian>
2024-12-27 19:57:48 +0100

    debut foction pour recuperer les donnees sit metar def 2

--- Meteo_Aeroport/module_projet.py
+++ Meteo_Aeroport/module_projet.py
@@ -42,3 +42,18 @@ def recuperer_donnees_meteobleu(site_aeroport, number):
         # Affiche un message d'erreur si la requête échoue
         print("Erreur lors de la récupération des données:", response.status_code)
         return None
+def recuperer_donnees_metar(code_aeroport):
+     # Construction de l'URL avec le code de l'aéroport
+    url = f"https://metar-taf.com/fr/{code_aeroport}"
+    header = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    # Envoi de la requête HTTP pour récupérer la page
+    response = requests.get(url,headers=header)
+    # Vérification si la requête a réussi (code 200)
+    print(response)
+    if response.status_code == 200:
+        # Analyse du contenu HTML de la page    
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # Recherche de la div contenant les informations pour le jour spécifié
+        div = soup.find("div", id="metar")
+        aeroport_name=extr(r'<h1 >(.+?)</h1>',div)
+        print (aeroport_name)
--- Meteo_Aeroport/test_def1.py
+++ Meteo_Aeroport/test_def1.py
@@ -1,2 +1,2 @@
-from module_projet import recuperer_donnees_meteobleu
-print(recuperer_donnees_meteobleu("aéroport-international-d'angoulême---cognac_france_6694763","1"))
+from module_projet import recuperer_donnees_metar
+print(recuperer_donnees_metar("LFLX"))

9071277319544f0aa625b06fb25fa6959e56d145
etudiant <etudiant@debian>
2024-12-27 20:18:04 +0100

    Mis ajour du code et rajout de la fonction metar_taf

--- Meteo_Aeroport/module_projet.py
+++ Meteo_Aeroport/module_projet.py
@@ -59,3 +59,49 @@ def recuperer_donnees_meteobleu(site_aeroport, number):
     else:
         print(f"Div avec id=day {number} introuvable.")
         return None
+
+def recuperer_donnees_metar_taf(code_aeroport):
+    """
+    Extrait les données METAR/TAF d'une page.
+
+    Args:
+        code_aeroport (str): Code de l'aéroport (ICAO ou autre).
+
+    Returns:
+        dict: Dictionnaire contenant les données extraites.
+    """
+    url = f"https://metar-taf.com/fr?c=465068.17139.6&hl={code_aeroport}"
+    soup = get_html_page(url)
+    if not soup:
+        return None
+
+    data = {}
+    metar_div = soup.find('div', id='metar')
+    if metar_div:
+        # Nom de l'aéroport
+        airport_name_match = re.search(r'<h1 class="h4 mb-0 test-ellipsis">(.+?)</h1>', str(metar_div))
+        data['airport_name'] = airport_name_match.group(1).strip() if airport_name_match else None
+
+        # Température
+        temp_match = re.search(r'(\d+ \u00b0C)', str(metar_div))
+        data['temperature'] = temp_match.group(1).strip() if temp_match else None
+
+        # Visibilité
+        visibility_match = re.search(r'<h3 class="mb-0">(\d+ km)</h3>', str(metar_div))
+        data['visibility'] = visibility_match.group(1).strip() if visibility_match else None
+
+        # Vitesse et direction du vent
+        wind_speed_match = re.search(r'<h3 class="mb-0">(\d+ kt)</h3>', str(metar_div))
+        wind_dir_match = re.search(r'(\d{3}\u00b0)', str(metar_div))
+        data['wind_speed'] = wind_speed_match.group(1).strip() if wind_speed_match else None
+        data['wind_direction'] = wind_dir_match.group(1).strip() if wind_dir_match else None
+
+        # Altitude des nuages
+        cloud_alt_match = re.search(r'<h3 class="mb-0">(\d+ ft)</h3>', str(metar_div))
+        data['cloud_altitude'] = cloud_alt_match.group(1).strip() if cloud_alt_match else None
+
+        # Pression atmosphérique
+        pressure_match = re.search(r'<h3 class="mb-0">(\d+ hPa)</h3>', str(metar_div))
+        data['pressure'] = pressure_match.group(1).strip() if pressure_match else None
+
+    return data

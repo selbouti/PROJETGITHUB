46bf471161d243d4d6db3b3a0aca29deef1b77ba
etudiant <etudiant@debian>
2025-01-01 20:58:56 +0100

    dibugging

--- Meteo_Aeroport/test_modules_meteo.py
+++ Meteo_Aeroport/test_modules_meteo.py
@@ -1,5 +1,5 @@
 from module_projet import recuperer_donnees_metar_taf
 from module_projet import recuperer_donnees_meteobleu, get_code_aeroport
-#print(recuperer_donnees_meteobleu("aéroport-international-d'angoulême---cognac_france_6694763","2"))
+print(recuperer_donnees_meteobleu("","2"))
 #print(recuperer_donnees_metar_taf("LFJR"))
-print(get_code_aeroport('Paris-Charles-de-Gaulle'))
+#print(get_code_aeroport('Paris-Charles-de-Gaulle'))

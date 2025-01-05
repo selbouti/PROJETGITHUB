aca7e91e475e34aec653bc75dea8684c86665353
etudiant <etudiant@debian>
2025-01-05 21:13:32 +0100

    modif

--- html/script.js
+++ html/script.js
@@ -11,24 +11,24 @@ fetch('donnees_meteo.json')
         // Injecter les données dans la page HTML
         if (currentPage === "index.html") {
         console.log(data)
-        document.getElementById("airport_name").textContent = data.airport_name || "Non disponible";
-        document.getElementById("temperature_max").textContent = data.temperature_max || "Non disponible";
-        document.getElementById("temperature_min").textContent = data.temperature_min || "Non disponible";
-        document.getElementById("precipitation").textContent = data.precipitation || "Non disponible";
-        document.getElementById("ensoleillement").textContent = data.ensoleillement || "Non disponible";
-        document.getElementById("temperature").textContent = data.temperature || "Non disponible";
-        document.getElementById("visibility").textContent = data.visibility || "Non disponible";
-        document.getElementById("wind_speed").textContent = data.wind_speed || "Non disponible";
-        document.getElementById("wind_direction").textContent = data.wind_direction || "Non disponible";
-        document.getElementById("cloud_altitude").textContent = data.cloud_altitude || "Non disponible";
-        document.getElementById("pressure").textContent = data.pressure || "Non disponible";
-        document.getElementById("icone").textContent = data.icone || "Non disponible";
+        document.getElementById("airport_name").textContent = data["aujourd'hui"].airport_name || "Non disponible";
+        document.getElementById("temperature_max").textContent = data["aujourd'hui"].temperature_max || "Non disponible";
+        document.getElementById("temperature_min").textContent = data["aujourd'hui"].temperature_min || "Non disponible";
+        document.getElementById("precipitation").textContent = data["aujourd'hui"].precipitation || "Non disponible";
+        document.getElementById("ensoleillement").textContent = data["aujourd'hui"].ensoleillement || "Non disponible";
+        document.getElementById("temperature").textContent = data["aujourd'hui"].temperature || "Non disponible";
+        document.getElementById("visibility").textContent = data["aujourd'hui"].visibility || "Non disponible";
+        document.getElementById("wind_speed").textContent = data["aujourd'hui"].wind_speed || "Non disponible";
+        document.getElementById("wind_direction").textContent = data["aujourd'hui"].wind_direction || "Non disponible";
+        document.getElementById("cloud_altitude").textContent = data["aujourd'hui"].cloud_altitude || "Non disponible";
+        document.getElementById("pressure").textContent = data["aujourd'hui"].pressure || "Non disponible";
+        document.getElementById("icone").textContent = data["aujourd'hui"].icone || "Non disponible";
         }
          else if (currentPage === "demain.html") {
         console.log(data)
-        document.getElementById("airport_name2").textContent = data.airport_name2 || "Non disponible";
-        document.getElementById("temperature_max2").textContent = data.temperature_max2 || "Non disponible";
-        document.getElementById("temperature_min2").textContent = data.temperature_min2 || "Non disponible";
+        document.getElementById("airport_name2").textContent = data.airport_name || "Non disponible";
+        document.getElementById("temperature_max2").textContent = data.temperature_max || "Non disponible";
+        document.getElementById("temperature_min2").textContent = data.temperature_min || "Non disponible";
         document.getElementById("precipitation2").textContent = data.precipitation2 || "Non disponible";
         document.getElementById("ensoleillement2").textContent = data.ensoleillement2 || "Non disponible";
         document.getElementById("temperature2").textContent = data.temperature2 || "Non disponible";
